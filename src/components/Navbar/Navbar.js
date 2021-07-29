5c62145762eea7b03a2b0bbdff94ec49eb89a835
Andrew Ozeki <andrewozeki@gmail.com>
2021-07-30 00:19:43 +0900

    added: routing to project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.scss';
+
+const Navbar = () => {
+    return (
+        <div className="nav-container">
+            <h1>Navbar works!</h1>
+        </div>
+    )
+}
+
+export default Navbar;
